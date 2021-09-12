78dbc2e7cef9e82918af6611eb37c99cac24781e
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2021-09-12 15:42:26 +0200

    Configuration keys

--- gaomon-m10k.sh
+++ gaomon-m10k.sh
@@ -1,43 +1,64 @@
 #!/usr/bin/env bash
 #set -e
 
-MODEL="GAOMON Gaomon Tablet Pad pad"
-MODEL_LOADED=$(xsetwacom list|grep "$MODEL")
-if [ -z "$MODEL_LOADED" ]; then
-	printf "Tablet $MODEL not detected" | systemd-cat -t gaemon-m10k.sh -p err
+MODEL_PAD="GAOMON Gaomon Tablet Pad pad"
+MODEL_STRIP="GAOMON Gaomon Tablet Touch Strip pad"
+MODEL_STYLUS="GAOMON Gaomon Tablet stylus"
+MODEL_PAD_LOADED=$(xsetwacom list|grep "$MODEL_PAD")
+MODEL_STRIP_LOADED=$(xsetwacom list|grep "$MODEL_STRIP")
+MODEL_STYLUS_LOADED=$(xsetwacom list|grep "$MODEL_STYLUS")
+if [ -z "$MODEL_PAD_LOADED" ]; then
+	printf "$MODEL_PAD not detected" | systemd-cat -t gaemon-m10k.sh -p err
+	exit 1
+fi
+if [ -z "$MODEL_STRIP_LOADED" ]; then
+	printf "$MODEL_STRIP not detected" | systemd-cat -t gaemon-m10k.sh -p err
+	exit 1
+fi
+if [ -z "$MODEL_STYLUS_LOADED" ]; then
+	printf "$MODEL_STYLUS not detected" | systemd-cat -t gaemon-m10k.sh -p err
 	exit 1
 fi
 
 # button 1
-xsetwacom set "$MODEL" button 1 key G
+xsetwacom set "$MODEL_PAD" button 1 key G # gradient-tool
 
 # button 2
-xsetwacom set "$MODEL" button 2 key F
+xsetwacom set "$MODEL_PAD" button 2 key F # bucket-tool
 
 # button 3
-xsetwacom set "$MODEL" button 3 key Ctrl Z
+xsetwacom set "$MODEL_PAD" button 3 "key +ctrl +z -z -ctrl" # cancel
 
 # button 4
-xsetwacom set "$MODEL" button 8 key T
+xsetwacom set "$MODEL_PAD" button 8 key T
 
 # button 5
-xsetwacom set "$MODEL" button 9 key C
+xsetwacom set "$MODEL_PAD" button 9 key C
 
 # button middle
-xsetwacom set "$MODEL" button 10 key X
+xsetwacom set "$MODEL_PAD" button 10 key X
 
 # button 6
-xsetwacom set "$MODEL" button 11 key B
+xsetwacom set "$MODEL_PAD" button 11 key B # pencil-tool
 
 # button 7
-xsetwacom set "$MODEL" button 12 key Ctlr +
+xsetwacom set "$MODEL_PAD" button 12 key O # increase opacity
 
 # button 8
-xsetwacom set "$MODEL" button 13 key Ctlr -
+xsetwacom set "$MODEL_PAD" button 13 key I # decrease opacity
 
 # button 9
-xsetwacom set "$MODEL" button 14 key H
+xsetwacom set "$MODEL_PAD" button 14 "key +ctrl +plus -plus -ctrl" # zoom
 
 # button 10
-xsetwacom set "$MODEL" button 15 key I
+xsetwacom set "$MODEL_PAD" button 15 "key +ctrl +minus -minus -ctrl" # dezoom
+
+# Wheel up
+xsetwacom set "$MODEL_STRIP"  AbsWheelUp "key +ISO_Level3_Shift [ [ -ISO_Level3_Shift" # brush size decrease
+
+# Wheel down
+xsetwacom set "$MODEL_STRIP"  AbsWheelDown "key +ISO_Level3_Shift ] ] -ISO_Level3_Shift" # brush size increase
+
+# Stylus
+xsetwacom set "$MODEL_STYLUS" button 2 key E # eraser
 exit 0
